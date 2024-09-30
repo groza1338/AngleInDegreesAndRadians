e4ee94208780c8110b85789120ad583606305256
groza_1337 <groza_1337@mail.ru>
2024-09-30 11:20:52 +0300

    Подправил тестики

--- test/AngleTest.java
+++ test/AngleTest.java
@@ -8,37 +8,37 @@ public class AngleTest {
 
     @Test
     public void testAdd() {
-        Angle angle1 = new Angle(30);
-        Angle angle2 = new Angle(60);
+        Angle angle1 = new Angle(30, false);
+        Angle angle2 = new Angle(60, false);
         Angle result = angle1.add(angle2);
         assertEquals(90, result.getAngleInDegrees(), 0.001);
     }
 
     @Test
     public void testSubtract() {
-        Angle angle1 = new Angle(60);
-        Angle angle2 = new Angle(30);
+        Angle angle1 = new Angle(60, false);
+        Angle angle2 = new Angle(30, false);
         Angle result = angle1.subtract(angle2);
         assertEquals(30, result.getAngleInDegrees(), 0.001);
     }
 
     @Test
     public void testCopy() {
-        Angle angle1 = new Angle(45);
+        Angle angle1 = new Angle(45, false);
         Angle copy = angle1.copy();
         assertEquals(45, copy.getAngleInDegrees());
     }
 
     @Test
     public void testTypeOfAngle() {
-        Angle angle1 = new Angle(90);
+        Angle angle1 = new Angle(90, false);
         assertEquals(TypeOfAngle.Square, angle1.determineTypeOfAngle());
     }
 
     @Test
     public void testCompare() {
-        Angle angle1 = new Angle(30);
-        Angle angle2 = new Angle(60);
+        Angle angle1 = new Angle(30, false);
+        Angle angle2 = new Angle(60, false);
         assertTrue(angle1.compare(angle2) < 0);
     }
 }
