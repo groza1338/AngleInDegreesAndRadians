165d50de06eca4d178cf6fcf5ec887505451131a
groza_1337 <groza_1337@mail.ru>
2024-10-02 02:19:15 +0300

    Правлю github actions

--- src/ru/groza1337/Main.java
+++ src/ru/groza1337/Main.java
@@ -1,49 +1,11 @@
 package ru.groza1337;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("""
-                В чем хотите задать 1 угол:
-                1. В радианах
-                2. В градусах""");
-        int choice = sc.nextInt();  // Получаем выбор пользователя
-
-        // Определяем, является ли ввод радианами
-        boolean isInRadian;
-        if (choice == 1) {
-            isInRadian = true;
-        } else if (choice == 2) {
-            isInRadian = false;
-        } else {
-            throw new RuntimeException("Некорректный выбор.");
-        }
-        System.out.print("Введите значение угла: ");
-        int answer1 = sc.nextInt();
-        Angle angle1 = new Angle(answer1, isInRadian);
-
-        System.out.println("""
-                В чем хотите задать 2 угол:
-                1. В радианах
-                2. В градусах""");
-        choice = sc.nextInt();  // Получаем выбор пользователя
-
-        // Определяем, является ли ввод радианами
-        if (choice == 1) {
-            isInRadian = true;
-        } else if (choice == 2) {
-            isInRadian = false;
-        } else {
-            throw new RuntimeException("Некорректный выбор.");
-        }
-        System.out.print("Введите значение угла: ");
-        int answer2 = sc.nextInt();
-        Angle angle2 = new Angle(answer2, isInRadian);
-
+        Angle angle = Angle.degrees(180);
+        Angle angle1 = Angle.radians(2 * Math.PI);
+        System.out.println(angle);
         System.out.println(angle1);
-        System.out.println(angle2);
-
+        System.out.println(angle.addRadians(angle1.getRadians()));
     }
 }
