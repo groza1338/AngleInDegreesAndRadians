679763b1678254bb01b573808540fa1d67c8513c
groza_1337 <groza_1337@mail.ru>
2024-10-02 02:06:54 +0300

    Проверка тестами при пуше

---
+++ test/ru/groza1337/AngleTest.java
@@ -0,0 +1,53 @@
+package ru.groza1337;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AngleTest {
+
+    @Test
+    void getDegrees() {
+        Angle angle = Angle.degrees(30);
+        assertEquals(30, angle.getDegrees());
+    }
+
+    @Test
+    void getRadians() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void toStringInRadians() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addRadians() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void subtractRadians() {
+    }
+
+    @Test
+    void compare() {
+    }
+
+    @Test
+    void compareWithRadians() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
